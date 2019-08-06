3947f3fb4e51e4870d0a17caf4c936683da11940
Noah Henry <noah.henry@allscripts.com>
2019-08-06 09:04:37 -0500

    03_02 - Structural directives: ngFor.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component'
+import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
 
 @NgModule({
   imports: [
@@ -10,7 +11,8 @@ import { MediaItemComponent } from './media-item.component'
   ],
   declarations: [
     AppComponent,
-    MediaItemComponent
+    MediaItemComponent,
+    MediaItemListComponent
   ],
   bootstrap: [
     AppComponent
