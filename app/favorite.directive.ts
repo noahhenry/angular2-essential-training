331f288d5ea6ec38cdf6f0563e34fcfb22d9f927
Noah Henry <noah.henry@allscripts.com>
2019-08-06 11:50:14 -0500

    03_07 - Angular pipes: built in.

--- app/favorite.directive.ts
+++ app/favorite.directive.ts
@@ -6,12 +6,14 @@ import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
   @HostBinding('class.is-favorite-hovering') hovering = false;
+
   @HostListener('mouseenter') onMouseEnter() {
     this.hovering = true;
   }
   @HostListener('mouseleave') onMouseLeave() {
     this.hovering = false;
   }
+  
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
   }
